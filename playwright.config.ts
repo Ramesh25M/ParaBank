965816e6be819d3f72bd685d5c39167457bab9d5
Ramesh Babu Mudragadda <rameshbabumudragadda@Rameshs-MacBook-Air.local>
2024-12-16 19:07:25 +0530

    change config to headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless : false,
+    headless : true,
 
     actionTimeout: 10_000,
     navigationTimeout: 30_000,
